0c634aa041112ea8fc58193c3e48e3c395dd991d
lhbdhr <17420553+lhbdhr@users.noreply.github.com>
2025-04-08 04:28:17 +0800

    Fix: fixed dkim body hash did not verify

--- app/mail_sender.py
+++ app/mail_sender.py
@@ -280,7 +280,7 @@ def sl_sendmail(
     send_request = SendRequest(
         envelope_from,
         envelope_to,
-        message_format_base64_parts(msg),
+        msg,
         mail_options,
         rcpt_options,
         is_forward,
